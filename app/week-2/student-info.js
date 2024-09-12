1a7750ca21ad2af0ebc0d23a646d515f653e5f76
Skylar4 <95008499+Skylar4@users.noreply.github.com>
2024-09-12 12:41:47 -0600

    Completed assignment 2
    created the week 2 assignment page. attached the link to git. Also edited the root page to meet specifications.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <p>Skylar Jones</p>
+            <Link href="https://github.com/Skylar4/cprg306-assignments.git"> GIT Link</Link>
+        </div>
+    );
+}
